2ba7912b601e6b94b845f552cac408368a64ed16
rogerando <rogenandreas@gmail.com>
2024-06-13 21:41:53 +0200

    added deleteFunction and length

--- symbolTable.c
+++ symbolTable.c
@@ -2,15 +2,16 @@
 #include <stdlib.h>
 #include <string.h>
 
+
 typedef struct Symbol{
     char token[100];
     int value;
     struct Symbol* next;
 } Symbol;
 
+int getLenght();
 
 Symbol* head = NULL;
-
 
 Symbol* createSymbol(const char* token, int value){
     Symbol* newSymbol = (Symbol*)malloc(sizeof(Symbol));
@@ -32,12 +33,50 @@ void insertSymbol(const char* token, int value){
 void displaySymbolTable(){
     Symbol* current = head;
     printf("\n*****Symbol Table*****\n");
+    int count = 1;
 
     while(current != NULL){
-        printf("Toeken: %s, Value: %d\n", current->token, current->value);
+        printf("%d) Token: %s, Value: %d\n", count, current->token, current->value);
+        current = current->next;
+        count++;
+    }
+
+    printf("Length: %d\n", getLenght());
+    printf("**********************\n\n");
+}
+
+void deleteSymbol(const char* token, int value){
+    Symbol* previous = NULL;
+    Symbol* current = head;
+
+    while(current != NULL && strcmp(current->token, token) != 0 && current->value != value){
+        previous = current;
         current = current->next;
     }
-    printf("\n");
+
+    if(current == NULL){
+        printf("Symbol not found!\n");
+        return;
+    }
+
+    if(previous == NULL){
+        head = current->next;
+    }else{
+        previous->next = current->next;
+    }
+
+    free(current);
+    printf("Symbol[Token: %s, Value: %d] has been deleted\n", token, value);
+}
+
+int getLenght(){
+    Symbol* current = head;
+    int count = 0;
+    while(current != NULL){
+        current = current->next;
+        count++;
+    }
+    return count;
 }
 
 
@@ -45,6 +84,10 @@ int main(){
     insertSymbol("Test", 1);
     insertSymbol("Hello", 2);
     insertSymbol("World", 2344);
+
+    displaySymbolTable();
+
+    deleteSymbol("Test", 1);
 
     displaySymbolTable();
 
